325c37a4e3599c9f000a9938bf9f09db96213041
yohualli <46802445+NestorMonroy@users.noreply.github.com>
2025-02-14 15:49:00 -0600

    update Style F-0.1.1

--- ui/styles/constants.py
+++ ui/styles/constants.py
@@ -1,5 +1,4 @@
-import re
-from typing import List, Dict
+from typing import Dict
 
 
 DEFAULT_THEME = 'cosmo'
@@ -51,75 +50,3 @@ STANDARD_THEMES = {
 
 # Temas de usuario
 USER_THEMES: Dict = {}
-
-class Keywords:
-    """Define las palabras clave y patrones para el sistema de estilos."""
-
-    COLORS: List[str] = [
-        "primary",
-        "secondary",
-        "success",
-        "info",
-        "warning",
-        "danger",
-        "light",
-        "dark",
-    ]
-
-    ORIENTS: List[str] = [
-        "horizontal",
-        "vertical"
-    ]
-
-    TYPES: List[str] = [
-        "outline",
-        "link",
-        "inverse",
-        "round",
-        "square",
-        "striped",
-        "focus",
-        "input",
-        "date",
-        "metersubtxt",
-        "meter",
-        "table"
-    ]
-
-    CLASSES: List[str] = [
-        "button",
-        "progressbar",
-        "checkbutton",
-        "combobox",
-        "entry",
-        "labelframe",
-        "label",
-        "frame",
-        "floodgauge",
-        "sizegrip",
-        "optionmenu",
-        "menubutton",
-        "menu",
-        "notebook",
-        "panedwindow",
-        "radiobutton",
-        "separator",
-        "scrollbar",
-        "spinbox",
-        "scale",
-        "text",
-        "toolbutton",
-        "treeview",
-        "toggle",
-        "tk",
-        "calendar",
-        "listbox",
-        "canvas",
-        "toplevel",
-    ]
-
-    # Compilar los patrones de expresiones regulares una sola vez
-    COLOR_PATTERN = re.compile("|".join(COLORS))
-    ORIENT_PATTERN = re.compile("|".join(ORIENTS))
-    CLASS_PATTERN = re.compile("|".join(CLASSES))
-    TYPE_PATTERN = re.compile("|".join(TYPES))
