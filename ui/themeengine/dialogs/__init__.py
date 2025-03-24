74219dd1d90a26a68fb24142b3779500fc1f42e4
yohualli <46802445+NestorMonroy@users.noreply.github.com>
2025-03-24 13:27:17 -0600

    add ColorValues

---
+++ ui/themeengine/dialogs/__init__.py
@@ -0,0 +1,15 @@
+"""
+    Módulo de clases base para diálogos personalizables.
+
+    Este módulo contiene diversas clases base de diálogos que pueden ser
+    utilizadas para crear diálogos personalizados para el usuario final.
+    Estas clases sirven como base para los métodos estáticos predefinidos
+    en las clases contenedoras `Messagebox` y `Querybox`.
+
+    Las clases incluidas en este módulo proporcionan:
+      - Estructura fundamental para crear ventanas modales
+      - Implementación de patrones comunes para interacción con usuarios
+      - Mecanismos para personalizar apariencia y comportamiento
+      - Gestión de eventos y respuestas del usuario
+
+"""
